37622e88dcbdb443d6aa59b0182e590a71d3b635
UncleKhab <cobrosoffice@gmail.com>
2021-01-06 21:14:06 +0200

    first commit

---
+++ music_constroller/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class CreateRoomPage extends Component {
+    constructor(props){
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                This is the room Create Page
+            </div>
+        )
+    }
+}
+
+export default CreateRoomPage
---
+++ music_constroller/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class RoomJoinPage extends Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <p>This is the room join page</p>
+            </div>
+        )
+    }
+}
+
+export default RoomJoinPage
+
